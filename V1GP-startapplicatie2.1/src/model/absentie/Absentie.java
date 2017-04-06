8bcc39d8cac1a8db95092cb1f8d74e7e026bb84b
Sijmen1 <sijmenj@gmail.com>
2017-04-06 16:01:16 +0200

    De grondverf van Absentie

---
+++ V1GP-startapplicatie2.1/src/model/absentie/Absentie.java
@@ -0,0 +1,14 @@
+package model.absentie;
+import model.persoon.*;
+import model.rooster.*;
+public class Absentie {
+	private Les deLes;
+	private Student leerling;
+	private boolean isAbsent, isZiek;
+	public Absentie(boolean absent, boolean ziek, Student stu, les ls){
+		isAbsent=absent;
+		ziek=isZiek;
+		leerling=stu;
+		les=ls;
+	}
+}
