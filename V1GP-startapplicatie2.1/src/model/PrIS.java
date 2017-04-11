edf30791c28a65f407c5f150306ca61c1d3b128f
Sijmen1 <sijmenj@gmail.com>
2017-04-11 13:43:39 +0200

    vewa

--- V1GP-startapplicatie2.1/src/model/PrIS.java
+++ V1GP-startapplicatie2.1/src/model/PrIS.java
@@ -221,14 +221,15 @@ public class PrIS {
 		String s="";
 		boolean firstString =true;
 		for (Absentie absentie : deAbsenties) {
+			if (absentie.getAbsentie() == true && firstString==false) {
+				s=s+":"+absentie.toString();
+			}
 			if (absentie.getStudentNummer() == studentNummer) {
 				if (absentie.getAbsentie() == true && firstString==true) {
 					s=s+absentie.toString();
 					firstString=false;
 				}
-				if (absentie.getAbsentie() == true && firstString==false) {
-					s=s+":"+absentie.toString();
-				}
+				
 			}
 
 		}
